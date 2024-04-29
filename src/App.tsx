555d5ec08aa7319c2e9ea9e1b2958ee47d608bbb
douglasjohnson <douglas.johnson4@gmail.com>
2024-04-29 20:38:02 +0100

    Add code formatting with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Vite + React</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
     </>
-  )
+  );
 }
 
 export default App;
